7dd16c887bd61e9b47d8460d78aa4c5117329bf9
victor1338 <victorlee1338@gmail.com>
2023-09-12 16:31:14 +0800

    as

---
+++ ProjA.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <time.h>
+#include <fstream>
+#include <cmath>
+#include <numeric>
+using namespace std;
+double const pi = 3.14159265358979323846;
+double const c = 2997924.58; //in armstrong per ps
+double const kb = 8.6173324 * pow(10, -5); //
+
+int main() {
+	return 0;
+
+}
